ddb6e347f68d0d7e20d8fdfd0e669c9b76b5c113
Brayden MacMillan <99274619+BraydenMacMillan7@users.noreply.github.com>
2022-03-13 17:32:32 +0000

    completed ultraviolence.html and tweeked index.html

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Ms Raffin All rights reserved
+//
+// Created by: Brayden MacMillan
+// Created on: Mar 11 2022
+// This file contains the JS functions for ultraviolence.html
+ 
+function myButtonClicked () {
+  // this function displays "Assign-01!"
+  
+  document.getElementById('Assign-01').innerHTML = 'I coded this in JavaScript! And, I encourage you to go stream Ultraviolence :)'
+}
